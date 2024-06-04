160a97442d7c14c4b3ca378a48ea0fce591a469e
Dylan <bollarodylan@gmail.com>
2024-06-04 19:04:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { POKEMONS } from '../data/mock-pokemons'
+import { Pokemon } from '../shared/models/pokemon'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+    pokemonList = POKEMONS
+
+    ngOnInit(): void {
+        console.table(this.pokemonList)
+        this.selectPokemon(this.pokemonList[0])
+    }
+
+    selectPokemon(pokemon: Pokemon) {
+        console.log(pokemon)
+    }
 }
