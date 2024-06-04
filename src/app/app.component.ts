6742ac5082d6579d470b6559b0f2a48e5f12a24a
Dylan <bollarodylan@gmail.com>
2024-06-04 21:57:52 +0200

    Creating search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,24 @@ import { Pokemon } from '../shared/models/pokemon'
 export class AppComponent implements OnInit {
     pokemonList: Pokemon[] = POKEMONS
     pokemonSelected: Pokemon | undefined
+    pokemonSelectedBySearch: Pokemon[] | undefined
 
     ngOnInit(): void {
         console.table(this.pokemonList)
     }
 
-    selectPokemon(pokemonId: string) {
-        const pokemon: Pokemon | undefined = this.pokemonList.find(
-            (pokemon) => pokemon.id === +pokemonId
-        )
+    selectPokemon(pokemon: Pokemon) {
+        console.log(pokemon)
+    }
 
-        this.pokemonSelected = pokemon ? pokemon : undefined
+    selectPokemonBySearch(str: string) {
+        if (!str) this.pokemonSelectedBySearch = undefined
+
+        const filteredPokemonList: Pokemon[] = this.pokemonList.filter(
+            (pokemon) => pokemon.name.toLowerCase().includes(str.toLowerCase())
+        )
+        this.pokemonSelectedBySearch = filteredPokemonList
+            ? filteredPokemonList
+            : undefined
     }
 }
